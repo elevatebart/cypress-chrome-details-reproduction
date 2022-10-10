66f1243ce1372ea9be16874e32923b1e199e0b66
elevatebart <bart@cypress.io>
2022-10-10 13:31:15 -0500

    make one test fail

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,8 +1,6 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('My First Test', () => {
+describe('Details', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.contains('hidden item').should('not.be.visible')
   })
 })
--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,10 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <details>
+    <summary>click me</summary>
+    <span>hidden item</span>
+  </details>  
 </template>
 
 <style scoped>
